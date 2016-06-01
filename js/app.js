2253afd1db8b1bbe18b196db559b1176fb52217b
Thomas Worns <tworns@yahoo.com>
2016-06-01 11:03:35 -0400

    added css and js folders

---
+++ js/app.js
@@ -0,0 +1,37 @@
+//$(document).foundation()
+
+var buildColorDiv = function(color){
+    return '<div style = "height: 100px; width: 100px; background-color: ' + color + '"></div>' ;
+};
+
+var buildList = function(listValues) {
+  return '<dl>' +
+    '<dt>Name:</dt>'+
+    '<dd>'+ listValues.name+ '</dd>'+
+    '<dt>'+'Hair color: ' + listValues.haircolor+ '</dt>'+
+    '<dd id = outColor>'+ listValues.colorDiv + '</dd>' +
+    '<dt>'+"Age: "+ '</dt>' +
+    '<dd>'+listValues.age +'</dd>'+
+    '<dt>'+ "Birthplace:  "+ '</dt>'+
+    '<dd>'+ listValues.birthplace+ '</dd>' +
+  '</dl>';
+};
+
+  var addValuesToDetails = function(ev){
+    ev.preventDefault();
+      var details = document.querySelector('form');
+
+      var colorDiv1 = buildColorDiv(this.hair_color.value);
+      var values = {
+            name : this.name.value, //this = e.currentTarget
+            haircolor : this.hair_color.value,
+            age : this.age.value,
+            colorDiv: colorDiv1,
+            birthplace : this.birthplace.value,
+      };
+    details.innerHTML +=  buildList(values);
+
+
+  };
+
+      document.querySelector('form').onsubmit = addValuesToDetails; // function(e) {
